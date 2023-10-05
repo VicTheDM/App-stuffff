ccb63dd257be8794d986a848a7963d0e8517b694
VicTheDM <victorsandovalsoftwaredev@gmail.com>
2023-10-05 16:52:09 -0600

    7

--- src/app/components/prime-ng.module.ts
+++ src/app/components/prime-ng.module.ts
@@ -8,16 +8,23 @@ import { TableModule } from 'primeng/table';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { ConfirmationService } from "primeng/api";
 import { MessageService } from "primeng/api";
+import { SidebarModule } from 'primeng/sidebar';
 
+import { FeatherModule } from 'angular-feather';
+import { allIcons } from 'angular-feather/icons';
 @NgModule({
-  imports: [   
+  imports: [
+    FeatherModule.pick(allIcons),
+    
   ],
   exports: [
     TableModule,
+    SidebarModule,
     ConfirmDialogModule,
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeatherModule,
   ],
   providers: [MessageService, ConfirmationService],
   declarations: [  ]
--- src/app/components/shared/menu/menu.component.ts
+++ src/app/components/shared/menu/menu.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent  implements OnInit{
+  visibleSidebar = false;
+  public fecha                    : any;
 
+  public nombre                   : string = 'Pepe';
+  public rol                      : string = 'Jefe';
+
+  public isCollapsed               = true;
+  public isCollapsed2              = true;
+  public isCollapsed3              = true;
+  public isCollapsed4              = true;
+  public isCollapsed5              = true;
+  public isCollapsed9              = true;
+  public SECTIONS : any = {
+    "documentos": true,
+    "perfil": true,
+    "reportes": true,
+    "reportesUsuario": true,
+    "reportesDocumentos": true,
+    "reportesGeneral": true,
+    "correo": true,
+    "participantes": true,
+  };
+  constructor(
+    private primengConfig: PrimeNGConfig
+    ) {
+    this.fecha = new Date().toLocaleString();
+
+  }
+  avatar() {
+    const
+      avatarDefault = 'https://img.icons8.com/windows/56/ffffff/user.png',
+      avatar        = avatarDefault;
+      // localStorage['userAvatar'] ? localStorage['userAvatar'] : 
+    return avatar;
+  }
+
+  
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    setTimeout(() => {
+        // const permitions = this.UserL.permissions;
+        this.SECTIONS = { //Si la sentencia no se cumple se muestra como se haya programado
+          "documentos": true,
+          "perfil": true,
+          "reportes": true,
+          "reportesUsuario": true,
+          "reportesDocumentos": true,
+          "reportesGeneral": true,
+          "correo": true,
+          "participantes": true,
+        }
+      }, 1000);
+  }
 }
